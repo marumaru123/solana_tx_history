51ff5192a778a4afb6c4b3d545e3bf2ab0731016
a b <ogsn@outlook.jp>
2021-05-15 06:52:39 +0000

    a

--- solana_tx_hist.js
+++ solana_tx_hist.js
@@ -11,14 +11,6 @@ let record = [];
 const maximumFractionDigits = 9;
 
 const tokenMap = JSON.parse(fs.readFileSync('./token.json', 'utf8'));
-/*
-const tokenMap = {
-	'4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R':'RAY',
-	'8PMHT4swUMtBzgHnh5U564N5sjPSiUz2cjEQzFnnP1Fo':'ROPE',
-	'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':'USDC',
-	'6SfhBAmuaGf9p3WAxeHJYCWMABnYUMrdzNdK5Stvvj4k':'Orca ROPE/SOL'
-}
-*/
 
 let accountInfoMap = {};
 
@@ -33,7 +25,6 @@ async function fn3(sig) {
     }
     if (result.value != null) {
       if (result.value.data.program === 'spl-token') {
-	  //console.log(result.value.data.parsed.info.owner + "::" + process.argv[2]);
           if (result.value.data.parsed.info.owner === process.argv[2]) {
 	    return true;
 	  }
@@ -91,25 +82,26 @@ async function fnn() {
     record.push('txid');
     record.push('date time');
     record.push('fee');
-    record.push('feepayer');
     record.push('sol delta');
     record.push('sol post amount');
+    record.push('currency1');
+    record.push('currency1 delta');
+    record.push('currency1 post amount');
+    record.push('currency2');
+    record.push('currency2 delta');
+    record.push('currency2 post amount');
+    record.push('currency3');
+    record.push('currency3 delta');
+    record.push('currency3 post amount');
     console.log(record.join(','));
     const fetched = await con.getConfirmedSignaturesForAddress2(pubkey);
     const signatures = fetched.map(val => val['signature']);
     for (let i = 0; i < signatures.length ; i++ ) {
         const value2 = await con.getParsedConfirmedTransaction(signatures[i]);
-	//console.log(value2);
-        //console.log('');
-        //console.log('***** start *****');
-        //console.log(signatures[i]);
 	record.splice(0);
 	record.push(signatures[i]);
 	let dateTime = new Date(value2.blockTime * 1000);
 	record.push(dateTime.toString());
-        //console.log('');
-        //console.log('fee:' + await lamportsToSolString(value2.meta.fee));
-	//record.push(await lamportsToSolString(value2.meta.fee));
 	let fee123 = await lamportsToSolString(value2.meta.fee);
         for (let j = 0; j < value2.transaction.message.accountKeys.length; j++ ) {
 	    const value31 = value2.transaction.message.accountKeys[j];
@@ -122,13 +114,6 @@ async function fnn() {
 	    const maximumFractionDigits = 9
 	    const val = new Intl.NumberFormat("en-US", { maximumFractionDigits }).format(sol);
 	    const val222 = [key]; 
-		/*
-	    if (j = 0) {
-		const aiueo1 = ": Fee Payer";
-	    } else {
-		const aiueo1 = "";
-	    }
-	    */
 	    val222.push(": ");
             val222.push(await balanceDelta(delta,true));
 	    val222.push(":" + val); 
@@ -146,65 +131,32 @@ async function fnn() {
 	    }
 	    if (key === process.argv[2]) {
 	      if (j === 0) {
-	          //console.log(val222.join(''));
 		  record.push(fee123);
 		  record.push(await balanceDelta(delta,true));
 	          record.push(val); 
 	      } else {
-	          //console.log(val222.join(''));
 		  record.push(0);
 		  record.push(await balanceDelta(delta,true));
 	          record.push(val); 
 	      }
 	    }
-		/*
-	    const val222 = key + ":" + post; 
-	    let aiueo1
-	    if (j = 0) {
-		aiueo1 = ": Fee Payer";
-	    } else {
-		aiueo1 = "";
-	    }
-	    if (!value31.writable) {
-		val222.push(": Fee Payer");
-		val222 += ": Readonly";
-	    }
-	    if (value31.signer) {
-		val222 += ": Signer";
-	    }
-	    */
-	    //if (value2.transaction.message.instructions.find((ix) => ix.programId.equals(pubkey))) {
-//		val += ": Program";
-//	    }
-	    //await console.log(val222);
 	}
-        //console.log(value2);
-        //console.log(signatures[i]);
         const accouts = value2.transaction.message.accountKeys.map(aiueo => aiueo.pubkey.toString());
 	let preTokenBalances2 = {};
         if (value2.meta.preTokenBalances.length > 0) {
-            //console.log('');
             for (let j = 0; j < value2.meta.preTokenBalances.length; j++ ) {
 	        const value3 = value2.meta.preTokenBalances[j];
 	        const account2 = accouts[value3['accountIndex']];
 		if (await fn3(account2)) {
-		    //console.log(accouts[value3['accountIndex']] + ':' + 
-		    //value3['mint'] + ':' + 
-		    //value3.uiTokenAmount.uiAmount);
 		    preTokenBalances2[account2] = value3.uiTokenAmount.uiAmount;
 		}
             }
 	}
-	//console.log(preTokenBalances2);
 	if (value2.meta.postTokenBalances.length > 0) {
-            //console.log('');
             for (let j = 0; j < value2.meta.postTokenBalances.length; j++ ) {
 	        const value3 = value2.meta.postTokenBalances[j];
 	        const account2 = accouts[value3['accountIndex']];
 		if (await fn3(account2)) {
-		    //console.log(accouts[value3['accountIndex']] + ':' + 
-		    //value3['mint'] + ':' + 
-		    //value3.uiTokenAmount.uiAmount);
 	            let preAmount;
 		    if (preTokenBalances2[account2]) {
 			    preAmount = preTokenBalances2[account2];
@@ -228,11 +180,8 @@ async function fnn() {
 		}
             }
 	}
-	//console.log('***** end *****');
 	console.log(record.join(','));
     }
 }
 
-//console.log(tokenMap);
-//console.log(tokenMap['6SfhBAmuaGf9p3WAxeHJYCWMABnYUMrdzNdK5Stvvj4k']);
 fnn();
